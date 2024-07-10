df98f0ca964b6b49d8e6ee60f0ec41844a3294df
heqingying <heqingying@feiniu.com>
2024-07-10 16:29:45 +0800

    feat: add NewDO

--- polyfill.go
+++ polyfill.go
@@ -1,7 +1,10 @@
 package gen
 
 import (
+	"fmt"
 	tmpl "github.com/rockcookies/go-gen/internal/template"
+	"gorm.io/gorm"
+	"reflect"
 )
 
 type Tmpl struct {
@@ -37,5 +40,39 @@ func newTmpl() *Tmpl {
 		TableQueryStruct:            tmpl.TableQueryStruct,
 		TableQueryStructWithContext: tmpl.TableQueryStructWithContext,
 		TableQueryIface:             tmpl.TableQueryIface,
+	}
+}
+
+func NewDO(model interface{}) func(db *gorm.DB, tableName string, opts ...DOOption) *DO {
+	modelType := reflect.TypeOf(model)
+	if modelType.Kind() == reflect.Ptr {
+		modelType = modelType.Elem()
+	}
+
+	return func(db *gorm.DB, tableName string, opts ...DOOption) *DO {
+		d := DO{}
+
+		// Force to copy Statement
+		d.db = db.WithContext(db.Statement.Context)
+
+		config := &DOConfig{}
+		for _, opt := range opts {
+			if opt != nil {
+				if applyErr := opt.Apply(config); applyErr != nil {
+					panic(applyErr)
+				}
+			}
+		}
+
+		d.DOConfig = config
+
+		d.modelType = modelType
+		err := d.db.Statement.Parse(model)
+		if err != nil {
+			panic(fmt.Errorf("Cannot parse model: %+v\n%w", model, err))
+		}
+		d.tableName = d.db.Statement.Schema.Table
+
+		return &d
 	}
 }
